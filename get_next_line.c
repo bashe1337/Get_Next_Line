68f64886cbd91621c101d3920ca68de5fe878bdb
Bess Ashe <bashe@su-h6.21-school.ru>
2019-10-16 20:22:02 +0300

    some changes

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bashe <bashe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/27 20:50:08 by bashe             #+#    #+#             */
-/*   Updated: 2019/10/15 20:42:48 by bashe            ###   ########.fr       */
+/*   Updated: 2019/10/16 20:20:00 by bashe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,29 @@ int		get_output(int ret)
 	else
 		return (0);
 }
+
+char	*get_remain(char *remain)
+{
+	char	*p;
+	char	*str;
+
+	p = NULL;
+	str = ft_strnew(1);
+	if (remain)
+	{
+		if ((p = ft_strchr(remain, '\n')))
+		{
+			*p = '\0';
+			str = ft_strdup(remain);
+			p++;
+			ft_strcpy(remain, p);
+		}
+		else
+			str = ft_strdup(remain);
+	}
+	return (str);
+}
+
 int		get_line(const int fd, char **line, char *remain)
 {
 	int			ret;
@@ -31,10 +54,8 @@ int		get_line(const int fd, char **line, char *remain)
 	char		*tmp;
 
 	p = NULL;
-	*line = ft_strnew(1);
-	if (remain)
-		*line = ft_strdup(remain);
-	while (!p && ((ret = read(fd, buff, BUFF_SIZE)) != 0))
+	*line = get_remain(remain);
+	while (!p || ((ret = read(fd, buff, BUFF_SIZE)) != 0))
 	{
 		buff[ret] = '\0';
 		if ((p = ft_strchr(buff, '\n')))
@@ -44,13 +65,13 @@ int		get_line(const int fd, char **line, char *remain)
 			remain = ft_strcpy(remain, p);
 		}
 		*line = ft_strjoin(*line, buff);
+		tmp = *line;
+		ft_strdel(&tmp);
 	}
-	tmp = *line;
-	ft_strdel(&tmp);
 	return (get_output(ret));
 }
 
-t_gnl	*list_new_elem(int fd)
+t_gnl	*get_new_elem(int fd)
 {
 	t_gnl	*new_elem;
 
@@ -69,7 +90,7 @@ int		get_next_line(const int fd, char **line)
 	if (!line || fd < 0)
 		return (-1);
 	if (head == NULL)
-		head = list_new_elem(fd);
+		head = get_new_elem(fd);
 	tmp = head;
 	while (tmp != NULL)
 	{
@@ -78,7 +99,11 @@ int		get_next_line(const int fd, char **line)
 		tmp = tmp->next;
 	}
 	if (tmp == NULL)
-		tmp = list_new_elem(fd);
+	{
+		tmp = get_new_elem(fd);
+		tmp->next = NULL;
+		head->next = tmp;
+	}
 	return (get_line(tmp->fd, line, tmp->remain));
 }
 
@@ -92,21 +117,17 @@ int main(void)
 	fd2 = open("bruh.txt", O_RDONLY);
 	get_next_line(fd1, &line);
 	printf("%s\n\n", line);
-	printf("%d\n", get_next_line(fd1, &line));
-	get_next_line(fd2, &line);
-	printf("%s\n\n", line);
-	printf("%d\n", get_next_line(fd2, &line));
 	get_next_line(fd1, &line);
 	printf("%s\n\n", line);
-	printf("%d\n", get_next_line(fd1, &line));
-	// get_next_line(fd, &line);
+	// get_next_line(fd1, &line);
 	// printf("%s\n\n", line);
-	// printf("%d\n", get_next_line(fd, &line));
-	// get_next_line(fd, &line);
+	// get_next_line(fd1, &line);
 	// printf("%s\n\n", line);
-	// printf("%d\n", get_next_line(fd, &line));
-	// get_next_line(fd, &line);
-	// printf("%s\n\n", line);
-	// printf("%d\n", get_next_line(fd, &line));
+	get_next_line(fd2, &line);
+	printf("%s\n\n", line);
+	get_next_line(fd2, &line);
+	printf("%s\n\n", line);
+		get_next_line(fd1, &line);
+	printf("%s\n\n", line);
 	return (0);
 }
